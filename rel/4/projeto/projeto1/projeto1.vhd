1ec2ded664cce410adacd05e75281870ee488a02
ArthurTemporim <arthurrtl@gmail.com>
2016-09-21 11:03:15 -0300

    Projeto do laboratorio adicionado.

---
+++ rel/4/projeto/projeto1/projeto1.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity projeto1 is
+	port(
+		e : in  std_logic_vector (3 downto 0) := "0000"
+		s : out std_logic_vector (6 downto 0);
+	);
+end projeto1;
+
+architecture Behavioral of projeto1 is
+begin
+
+s(0) <= e(2) + 
+
+end Behavioral;
+
