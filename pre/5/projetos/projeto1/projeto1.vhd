a21a4b207ba4d92c5e4ecea380f23c0cbeeb8744
ArthurTemporim <arthurrtl@gmail.com>
2016-09-24 12:24:11 -0300

    Alteração no multiplex. 4->1

--- pre/5/projetos/projeto1/projeto1.vhd
+++ pre/5/projetos/projeto1/projeto1.vhd
@@ -3,40 +3,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity projeto1 is
 	port (
-		e   : in  std_logic_vector (7 downto 0) := "00000000";
-		sel : in  std_logic_vector (2 downto 0) := "000";
-		s   : out std_logic
+		m     : in  std_logic_vector (3 downto 0) := "00000000"; -- Multiplex 4->1
+		sel   : in  std_logic_vector (1 downto 0) := "000";      -- Seletora
+		d     : in  std_logic := '0';										-- Demux 1->4
+		s_m   : out std_logic
 	);
 end projeto1;
 
 architecture Behavioral of projeto1 is
 
---signal aux : std_logic_vector (3 downto 0);
-signal saida_sel : std_logic;
+signal saida_mux : std_logic;
 
 begin
 
--- Multiplexador 8 para 1.
-process (e, sel)
+-- Multiplexador 4 para 1.
+process (m, sel)
 begin
-	if(sel = "000") then
-		saida_sel <= e(0);
-	elsif(sel = "001") then
-		saida_sel <= e(1);
-	elsif(sel = "010") then
-		saida_sel <= e(2);
-	elsif(sel = "011") then
-		saida_sel <= e(3);
-	elsif(sel = "100") then
-		saida_sel <= e(4);
-	elsif(sel = "101") then
-		saida_sel <= e(5);
-	elsif(sel = "110") then
-		saida_sel <= e(6);
+	if(sel = "00") then
+		saida_mux <= m(0);
+	elsif(sel = "01") then
+		saida_mux <= m(1);
+	elsif(sel = "10") then
+		saida_mux <= m(2);
 	else
-		saida_sel <= e(7);
+		saida_mux <= m(3);
 	end if;
 end process;
+s_m <= saida_mux;
+
 
 end Behavioral;
 
