d4257f09d0ddb886ae4a0b0e786de68f63f294d0
Eduardo Nunes <eduardonunes2525@gmail.com>
2016-09-26 23:24:59 -0300

    Fazendo relatorio 04

--- rel/4/projeto/projeto1/teste02.vhd
+++ rel/4/projeto/projeto1/teste02.vhd
@@ -1,19 +1,19 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity projeto1 is
+entity fdsa is
 	port (
 		e : in  std_logic_vector (3 downto 0):= "0100";
 		s: out std_logic_vector (6 downto 0)
 	);
-end projeto1;
+end fdsa;
 
-architecture Behavioral of projeto1 is
+architecture Behavioral of fdsa is
 
 begin
-
-
-s <= "1111110" when e = "0000" else
+	process (e)
+	begin
+s <= 
 	  "0110000" when e = "0001" else
 	  "1101101" when e = "0010" else
 	  "1111001" when e = "0011" else
@@ -22,6 +22,7 @@ s <= "1111110" when e = "0000" else
 	  "1011111" when e = "0110" else
 	  "1110000" when e = "0111" else
 	  "1111111" when e = "1000" else
-	  "1111011" when e = "1001" else
+	  "1111011" when e = "1001" ;
+	end process;
 end Behavioral;
 
