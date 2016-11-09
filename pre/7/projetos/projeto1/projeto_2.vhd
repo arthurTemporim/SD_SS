eb7c354decad4c53c9dca44cbfd3e5ba63f2a8ce
ArthurTemporim <arthurrtl@gmail.com>
2016-11-09 00:31:27 -0200

    Projeto 2 finalizado.

--- pre/7/projetos/projeto1/projeto_2.vhd
+++ pre/7/projetos/projeto1/projeto_2.vhd
@@ -14,17 +14,26 @@ end projeto_2;
 
 architecture Behavioral of projeto_2 is
 
+signal auxEntrada : std_logic_vector (2 downto 0);
+
 begin
 	process (clk, reset)
-		variable contagem : integer range 0 to 6;
-		begin
+		begin		
 			if (reset = '1') then
-				contagem := 0;
+				auxEntrada <= "000";
 			elsif (rising_edge(clk)) then
 				if (enable = '1') then
-					contagem := contagem + 1;
+					if (entrada = "000") then
+						auxEntrada <= "010";
+					elsif (entrada = "010") then
+						auxEntrada <= "101";
+					elsif (entrada = "101") then
+						auxEntrada <= "110";
+					else 
+						auxEntrada <= "000";
+					end if;
 				end if;
-			end if;
-		saida <= conv_std_logic_vector(contagem,3);
-	end process;
+			end if;			
+			saida <= auxEntrada;
+	end process;		
 end Behavioral;
