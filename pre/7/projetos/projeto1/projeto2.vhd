b46784ff1cc7c528b70705c96afe36fd99db61d2
ArthurTemporim <arthurrtl@gmail.com>
2016-11-08 23:08:33 -0200

    Entradas e sinais criados.

--- pre/7/projetos/projeto1/projeto2.vhd
+++ pre/7/projetos/projeto1/projeto2.vhd
@@ -3,12 +3,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity projeto2 is
 	port(
-		clock : in std_logic
+		clock : in std_logic;
+		a : in std_logic := '0';
+		b : in std_logic := '0';
+		c : in std_logic := '0';
+		sa : out std_logic;
+		sb : out std_logic;
+		sc : out std_logic
 	);
 end projeto2;
 
 architecture Behavioral of projeto2 is
 
+-- SIGNALS
+signal clear : std_logic;
+signal inA : std_logic;
+signal outA : std_logic;
+signal outB : std_logic;
+signal outC : std_logic;
+
+-- COMPONENTS
 component flip_flop_jk
    port( J,K: in  std_logic;
          Reset: in std_logic;
@@ -18,9 +32,9 @@ component flip_flop_jk
 end component;
 
 begin
-	f1 : flip_flop_jk port map ('1','1','0','1', clock);
-	--f2 : flip_flop_jk port map ('1','1','0','1', clock);
-	--f3 : flip_flop_jk port map ('1','1','0','1', clock);
+	f1 : flip_flop_jk port map (inA,inA,clear,'1', clock);
+	f2 : flip_flop_jk port map (outA,outA,clear,'1', clock);
+	f3 : flip_flop_jk port map (outB,outB,clear,'1', clock);
 
 
 end Behavioral;
