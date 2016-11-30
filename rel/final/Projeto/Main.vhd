65238598397a81e83a9ad17ca9a327f49697bd84
Mariana Pícolo <marianapicolo@hotmail.com>
2016-11-30 03:00:40 -0200

    Modos de uso incrementados.

--- rel/final/Projeto/Main.vhd
+++ rel/final/Projeto/Main.vhd
@@ -5,9 +5,12 @@ use ieee.numeric_std.all;
 entity Main is
 
 	port(
-		modo 		 : in std_logic							 := '0'; 		  -- Gera / Valida.
-		k			 : in std_logic_vector (3 downto 0)  := "1000"; 	  -- Seleciona um grupo de até 16 bits
-		clk		 : in  std_logic 							 := '0';			  -- Clock do circuito.
+		modo 		 : in std_logic							 := '0'; 		  -- Gera / Valida. 							 [sel 1]
+		k			 : in std_logic_vector (3 downto 0)  := "1000"; 	  -- Seleciona um grupo de até 16 bits. [sel 2~5]
+		s			 : in std_logic_vector (3 downto 0)  := "0000"; 	  -- Seleciona um valor.					 [sel 6~9]
+		hab_clk	 : in std_logic							 := '1';			  -- Habilitador de clock. 				 [sel 10]
+		but_clk   : in std_logic							 := '0';			  -- Botão pra trabalhar como clock.    [sel 11]
+		clk		 : in std_logic 							 := '0';			  -- Clock do circuito.
 		leds		 : out std_logic_vector (15 downto 0)					  -- LEDs de saída do circuito.
 	);
 		
@@ -16,12 +19,17 @@ end Main;
 architecture Behavioral of Main is
 
 	-- Vetor usado para deslocamento de bits.
-	signal vetor : std_logic_vector (15 downto 0) := "0000000000000010";
+	signal vetor 			: std_logic_vector (15 downto 0) := "0000000000000010";
+	-- Guarda quantas vezes o valor de 'S' aparece.
+	signal estatistica	: std_logic_vector (3 downto 0) 	:= "0000";
+	-- Conta quantas vezes o valor de 'S' aparece no vetor.
+	signal conta_s : integer range 0 to 15;
 	
 begin
 	
 	process (vetor, clk, modo)
-		variable grupo : integer range 0 to 15; 
+		-- Transforma o valor binário do grupo 'k' en inteiro.
+		variable grupo : integer range 0 to 15;
 	begin
 		-- Função GERA.
 		if (modo = '0') then
@@ -29,15 +37,21 @@ begin
 			grupo := to_integer(unsigned(k));
 			-- Aplica a gerção aleatória.
 			vetor(grupo) <= vetor(0) xor vetor(1);
-			if (clk'event and clk = '1') then			
+			-- Da o shift nos bits em borda de subida.
+			if (clk'event and clk = '1' and hab_clk = '1') then			
 				vetor <= std_logic_vector(unsigned(vetor) srl 1);
 			end if;
 		-- Função Valida.
-		else 
+		--else
+			-- Se os 4 últimos digitos do vetor foram iguais ao valor de 'S' então conta.
+			if (vetor(0) = s(0) and vetor(1) = s(1) and vetor(2) = s(2) and vetor(3) = s(3)) then
+				conta_s <= conta_s + 1;
+			end if;
 		end if;
 	end process;
-	
+	-- Atribui valor inteiro da contagem para sinal.
+	estatistica <= std_logic_vector(to_unsigned(conta_s, 4));
 	-- Atribui o valor do vetor deslocado aos LEDs de saida.
-	leds <= vetor; 
+	leds <= vetor;
 	
 end Behavioral;
